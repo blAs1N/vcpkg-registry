76ec09ec10bb18e80d2f07183e6ca7ca7c6304f6
blAs1N <qazasa123@gmail.com>
2021-04-19 22:21:35 +0900

    Uncomment

--- ports/bsbase/portfile.cmake
+++ ports/bsbase/portfile.cmake
@@ -14,8 +14,7 @@ vcpkg_configure_cmake (
 vcpkg_install_cmake ()
 vcpkg_fixup_cmake_targets ()
 
-message ("${CURRENT_PACKAGES_DIR}")
-# file (REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file (REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file (
 	INSTALL "${SOURCE_PATH}/LICENSE"
