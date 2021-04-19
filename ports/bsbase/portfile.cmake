d1d4b4e23bea6c36ea2cfcdfe82c775cf402b150
blAs1N <qazasa123@gmail.com>
2021-04-19 21:40:25 +0900

    Debug

--- ports/bsbase/portfile.cmake
+++ ports/bsbase/portfile.cmake
@@ -14,7 +14,8 @@ vcpkg_configure_cmake (
 vcpkg_install_cmake ()
 vcpkg_cmake_config_fixup ()
 
-file (REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+message ("${CURRENT_PACKAGES_DIR}")
+# file (REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file (
 	INSTALL "${SOURCE_PATH}/LICENSE"
