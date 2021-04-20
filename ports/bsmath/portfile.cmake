9314b9b164350dc388a7d07165fd7049536e9956
blAs1N <qazasa123@gmail.com>
2021-04-20 23:00:44 +0900

    Add BSMath (Temp)

---
+++ ports/bsmath/portfile.cmake
@@ -0,0 +1,29 @@
+vcpkg_from_github (
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO blAs1N/BSMath
+	REF 4dff6a260df09177858117dd733db72cbaae979e
+	SHA512 4078d38c12e99684013245fde9fa30fe90f1d74d9a268ff7c1ca53c9f90ea76638b31063e9965adb226ae9484c15fbb1c1147879f6a19af9e8f3161d4269083a
+	HEAD_REF master
+)
+
+vcpkg_configure_cmake (
+	SOURCE_PATH "${SOURCE_PATH}"
+	PREFER_NINJA
+)
+
+vcpkg_install_cmake ()
+
+file (REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(GLOB DEBUGS "${CURRENT_PACKAGES_DIR}/debug/")
+list(LENGTH DEBUGS DEBUG_LEN)
+
+if(DEBUG_LEN EQUAL 0)
+	file (REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+endif()
+
+configure_file (
+    "${SOURCE_PATH}/LICENSE"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright"
+    COPYONLY
+)
