ebd7c9537704694b9bbee7999dee6c78515f259e
blAs1N <qazasa123@gmail.com>
2021-04-20 20:47:40 +0900

    Add debug message

--- ports/bsbase/portfile.cmake
+++ ports/bsbase/portfile.cmake
@@ -14,6 +14,8 @@ vcpkg_configure_cmake (
 )
 
 vcpkg_install_cmake ()
+
+message ("Type: ${VCPKG_BUILD_TYPE}")
 vcpkg_fixup_cmake_targets ()
 
 file (REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
